ff1215b50572c611dfe473f3ff9c451fa297f7ad
MEMALKA <MEMALKA@MEMALKA-IL.il.oracle.com>
2018-10-08 12:26:54 +0300

    second line in new branch changesToSecond

--- SecondModule/src/Main.scala
+++ SecondModule/src/Main.scala
@@ -1,5 +1,6 @@
 object Main {
   def main (args : Array[String]): Unit = {
-    println("trying git in module secondModule")
+    println("trying git in module secondGitModule")
+    println("second line is new in branch changesToSecond")
   }
 }
